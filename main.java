15e1e00e3fc12f03508dd601f6ce0e7845659206
DarkAng10 <angelcarias4@gmail.com>
2023-05-19 10:30:17 +0200

    datos actualizados

---
+++ main.java
@@ -0,0 +1,9 @@
+public class Main {
+ public static void main(String... args) {
+   assert "Eva" == Data.students.get(0);
+   assert Admin.enroll("Pyhon", "Eva");
+   System.out.println("It's working!");
+ }
+}
+
+
