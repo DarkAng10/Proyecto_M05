1449dcb1e1a5d1e1536ac26ae8cc87467d2a8b68
DarkAng10 <angelcarias4@gmail.com>
2023-05-19 10:19:25 +0200

    Data.java subido

---
+++ data.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Data {
+ static List<String> courses = Arrays.asList("Python", "Java");
+ static List<String[]> enrollments = new ArrayList<>();
+ static List<String> students = Arrays.asList("Eva", "Marc");
+}
+
+
